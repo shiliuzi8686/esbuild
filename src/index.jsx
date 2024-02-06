99cb20807db08c03c2213d740866f8edc1f60744
shiliu <yuting2.liu@ly.com>
2024-02-06 16:27:02 +0800

    feat: add build、transform、serve、envPlugin、http-import-plugin、httpCdnBuild

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+// src/index.jsx
+// import Server from "react-dom/server";
+
+// let Greet = () => <h1>Hello, juejin!</h1>;
+// console.log(Server.renderToString(<Greet />));
+
+// react-dom 的内容全部从 CDN 拉取
+// 这段代码目前是无法运行的
+import { render } from "https://cdn.skypack.dev/react-dom";
+import React from 'https://cdn.skypack.dev/react'
+
+let Greet = () => <h1>Hello, juejin!</h1>;
+
+render(<Greet />, document.getElementById("root"));
